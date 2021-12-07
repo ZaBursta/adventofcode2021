f44ebb00b35b90b11f980744b9692a3e56bcdeb4
Zachary Burstain <zbiowa@gmail.com>
2021-12-07 14:58:33 -0800

    #Code Part 1
    It's lame I tried to be fancy, but this works

--- Day 3/power-supply/src/main.rs
+++ Day 3/power-supply/src/main.rs
@@ -1,69 +1,38 @@
-enum Direction {
-    Up,
-    Down,
-    Forward
-}
-struct Movement {
-    direction: Direction,
-    distance: i32,
-}
+fn part1(values: impl AsRef<[i32]>, binary_length: i32) {
+    let values = values.as_ref();
+    let mut output = 0;
 
-fn part1(movements: impl AsRef<[Movement]>) {
-    let mut depth = 0;
-    let mut dist = 0;
-
-    for movement in movements.as_ref() {
-        if matches!(movement.direction, Direction::Forward) {
-            dist += movement.distance;
-        } else {
-            depth += movement.distance * if matches!(movement.direction, Direction::Up) { -1 } else { 1 };
+    for i in 0..binary_length {
+        let sig_fig_bit = 2_i32.pow((binary_length - i - 1).try_into().unwrap());
+        let sum: i32 = values.iter().map(|x| x & sig_fig_bit).sum();
+        if sum > (sig_fig_bit * values.len() as i32/2) {
+            output += sig_fig_bit;
         }
     }
-
-    println!("Depth of {}, distance of {}, product is {}", depth, dist, depth*dist);
+    let max_value = 2_i32.pow((binary_length).try_into().unwrap()) - 1;
+    println!("Gamma Rate: {} Epsilon Rate: {}", output, !output & max_value);
+    println!("Power Consupmtion: {}", output * (!output & max_value));
 }
 
-fn part2(movements: impl AsRef<[Movement]>) {
-    let mut depth = 0;
-    let mut dist = 0;
-    let mut aim = 0;
-
-    for movement in movements.as_ref() {
-        if matches!(movement.direction, Direction::Forward) {
-            dist += movement.distance;
-            depth += movement.distance * aim;
-        } else {
-            aim += movement.distance * if matches!(movement.direction, Direction::Up) { -1 } else { 1 };
-        }
-    }
-
-    println!("Using Aim: Depth of {}, distance of {}, product is {}", depth, dist, depth*dist);
-}
  fn main() {
-
     let input = include_str!("../input.txt");
-
-    let values: Vec<Movement> = input
+    let values: Vec<i32> = input
         .lines()
-        .filter_map(|x| {
-            let x = x.trim();
-            if !x.is_empty() {
-                let (direction, distance) = x.split_once(' ').expect("");
-                Some(Movement{
-                    direction: match direction {
-                        "down" => Direction::Down,
-                        "up" => Direction::Up,
-                        "forward" => Direction::Forward,
-                        _ => Direction::Forward,
-                    },
-                    distance:distance.parse().unwrap()
-                })
+        .filter_map(|line| {
+            let line = line.trim();
+            if !line.is_empty() {
+                let mut value = 0;
+                for (i, c) in line.chars().enumerate() {
+                    if c == '1' {
+                        value += 2_i32.pow((line.len() - i - 1).try_into().unwrap());
+                    }
+                }
+                // println!("{} is {}", line, value);
+                Some(value)
             } else {
                 None
             }
         })
         .collect();
-
-    part1(&values);
-    part2(&values);
+    part1(&values, 12);
 }
