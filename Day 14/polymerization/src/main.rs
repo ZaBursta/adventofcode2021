aa3301048cac1f8d31d859747cd77674f9559508
Zachary Burstain <zbiowa@gmail.com>
2021-12-14 10:28:49 -0800

    Code Part 2
    (it's even faster to do 40 steps than 10!)

--- Day 14/polymerization/src/main.rs
+++ Day 14/polymerization/src/main.rs
@@ -14,6 +14,21 @@ fn do_step(polymer: String, reaction_map: &HashMap<(char, char), char>) -> Strin
     return output_polymer;
 }
 
+fn do_step_memory_optimized(polymer: &HashMap<(char, char), u128>, reaction_map: &HashMap<(char, char), char>) -> HashMap<(char, char), u128> {
+    let mut new_polymer: HashMap<(char, char), u128> = HashMap::new();
+
+    for (pair, count) in polymer {
+        if reaction_map.contains_key(pair) {
+            let inserted_char = reaction_map.get(pair).unwrap();
+            *new_polymer.entry((pair.0, *inserted_char)).or_insert(0) += count;
+            *new_polymer.entry((*inserted_char, pair.1)).or_insert(0) += count;
+        } else {
+            *new_polymer.entry(*pair).or_insert(0) += count;
+        }
+    }
+    return new_polymer;
+}
+
 fn find_most_and_least_common_char_count_in_string(string: String) -> (u32, u32) {
     let mut char_counts: [u32; 26] = [0; 26];
     for char in string.chars() {
@@ -32,13 +47,42 @@ fn find_most_and_least_common_char_count_in_string(string: String) -> (u32, u32)
     return (max, min);
 }
 
-fn part2(start_polymer: &str, reaction_map: &HashMap<(char, char), char>) {
-    let mut polymer = start_polymer.to_string();
-    for _ in 0..40 {
-        polymer = do_step(polymer, reaction_map);
+fn find_most_and_least_common_char_count_in_map(start_char: char, end_char: char, map: &HashMap<(char, char), u128>) -> (u128, u128) {
+    let mut char_counts: [u128; 26] = [0; 26];
+
+    for (pair, count) in map {
+        char_counts[(pair.0 as u32 - 65) as usize] += count;
+        char_counts[(pair.1 as u32 - 65) as usize] += count;
     }
 
-    let (most_common_count, least_common_count) = find_most_and_least_common_char_count_in_string(polymer);
+    char_counts[(start_char as u32 - 65) as usize] += 1;
+    char_counts[(end_char as u32 - 65) as usize] += 1;
+
+    let mut min = u128::MAX;
+    let mut max = u128::MIN;
+
+    for char_count in char_counts {
+        max = cmp::max(char_count/2, max);
+        if char_count != 0 {
+            min = cmp::min(char_count/2, min);
+        }
+    }
+
+    return (max, min);
+}
+
+fn part2(start_polymer: &str, reaction_map: &HashMap<(char, char), char>) {
+    let mut polymer: HashMap<(char, char), u128> = HashMap::new();
+    for window in start_polymer.chars().collect::<Vec<char>>().windows(2) {
+        *polymer.entry((window[0], window[1])).or_insert(0) += 1;
+    }
+
+    for _ in 0..40 {
+        polymer = do_step_memory_optimized(&polymer, reaction_map);
+    }
+
+    start_polymer.chars().next().unwrap();
+    let (most_common_count, least_common_count) = find_most_and_least_common_char_count_in_map(start_polymer.chars().next().unwrap(), start_polymer.chars().last().unwrap(), &polymer);
     println!("Difference between most and least common element after 40 steps is {}", most_common_count - least_common_count);
 }
 
